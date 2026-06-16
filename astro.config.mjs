0a358afe38d6524263659f3c029830bbbf26d60b
Centeno Fernando <fgc_710@hotmail.com>
2026-06-16 13:44:54 -0300

    Set Astro base and site URL for GitHub Pages project deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://metidoenmicancion.github.io/MiPorfolio/',
+  base: '/MiPorfolio/',
   integrations: [tailwind()]
 });
